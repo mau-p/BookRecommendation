09a61fa0eb3b4cb656c2a936f80fc49ecfc71a1a
mau-p <thisismyemail.me@pm.me>
2021-12-27 12:47:50 +0100

    Made GUI slightly better

--- GUI.py
+++ GUI.py
@@ -5,20 +5,25 @@ root = tk.Tk()
 root.title("Bookrecommendation")
 
 root.grid()
+root.geometry('960x600')
 
 text0 = ("Welcome to Bookrecommendation! \n \n"
 "This knowledge system uses expert knowledge to give you a book recommendation. \n"
-"Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n"
-"With expert knowledge from ... \n")
-text1 = ("text1")
-text2 = ("text2")
-text3 = ("text3")
+"Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n")
+text1 = ("How old are you?")
+text2 = ("What is your gender?")
+text3 = ("What is your reading level?")
 text4 = ("text4")
 
 text = tk.Label(root, text=text0)
-text.grid(column=0, row=0, columnspan=3, padx=5, pady=(10,0))
+text.place(relx=.5, rely=.3, anchor='center')
 
 progress_list = [text0, text1, text2, text3, text4]
+
+def place_buttons():
+    text.place(relx=.5, rely=.3, anchor='center')
+    button_previous.place(relx=.1, rely=.9, anchor='center')
+    button_next.place(relx=.9, rely=.9, anchor='center')
 
 def previous(progress_number):
     global text
@@ -32,32 +37,26 @@ def previous(progress_number):
 
     if progress_number == 1:
         button_previous = tk.Button(root, text="Previous", state=DISABLED)
-
-    text.grid(column=0, row=0, columnspan=3, padx=5, pady=(10,0))
-    button_previous.grid(column=0, row=10, pady=10)
-    button_next.grid(column=2, row=10, pady=10)
+    
+    place_buttons()
 
 def next(progress_number):
     global text
     global button_previous
     global button_next
-    
-    text.grid_forget()
+
+    text.place_forget()
     text = tk.Label(root, text=progress_list[progress_number-1])
     button_previous = tk.Button(root, text="Previous", command=lambda: previous(progress_number-1))
     button_next = tk.Button(root, text="Next", command=lambda: next(progress_number+1))
 
-    if progress_number == 5:
+    if progress_number == len(progress_list):
         button_next = tk.Button(root, text="Next", state=DISABLED)
 
-    text.grid(column=0, row=0, columnspan=3, padx=5, pady=(10,0))
-    button_previous.grid(column=0, row=10, pady=10)
-    button_next.grid(column=2, row=10, pady=10)
+    place_buttons()
 
-button_previous = tk.Button(root, text="Previous", command=previous, state=DISABLED).grid(column=0, row=10, pady=10)
-button_quit = tk.Button(root, text="Quit", command=root.quit).grid(column=1, row=10, pady=10)
-button_next = tk.Button(root, text="Next", command=lambda: next(2)).grid(column=2, row=10, pady=10)
+button_previous = tk.Button(root, text="Previous", command=previous, state=DISABLED).place(relx=.1, rely=.9, anchor='center')
+button_quit = tk.Button(root, text="Quit", command=root.quit).place(relx=.5, rely=.9, anchor='center')
+button_next = tk.Button(root, text="Next", command=lambda: next(2)).place(relx=.9, rely=.9, anchor='center')
 
 root.mainloop()
-
-# Not sure how to implement a GUI yet, but that will be figured out. 
