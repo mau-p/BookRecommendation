101b5833780d71aba5720013892ebc3bf9789ae0
mau-p <thisismyemail.me@pm.me>
2021-12-29 15:28:06 +0100

    Added buttons (but no functions)

--- GUI.py
+++ GUI.py
@@ -1,26 +1,32 @@
 import tkinter as tk
-from tkinter.constants import CENTER
+from tkinter.constants import CENTER, DISABLED
 
+# Basic properties for all windows
 class Window:
-    def basic_layout(self, root):
-        geometry = root.geometry("960x600")
-        return geometry
+    def __init__(self) -> None:
+        self.geometry = root.geometry("960x600")
+        self.previous = tk.Button(root, text="Previous")
+        self.next = tk.Button(root, text="Next")
+        self.previous.place(relx=.1, rely=.9)
+        self.next.place(relx=.85, rely=.9)
 
 # The window that is displayed when the user first launches the program
 class IntroWindow(Window):
     def __init__(self) -> None:
-        root.geometry = self.basic_layout(root)
+        Window.__init__(self)
         self.text = tk.Label(root, text=("Welcome to Bookrecommendation! \n \n"
-            "This knowledge system uses expert knowledge to give you a book recommendation. \n"
-            "Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n"))
+                                         "This knowledge system uses expert knowledge to give you a book recommendation. \n"
+                                         "Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n"))
         self.text.place(relx=.5, rely=.3, anchor=CENTER)
+        self.previous = tk.Button(root, text="Previous", state=DISABLED)
+        self.previous.place(relx=.1, rely=.9)
 
 
 # Type of window that asks the user for value (for example their age or a book rating)
 class IntegerWindow(Window):
     def __init__(self, question: str) -> None:
-        root.geometry = self.basic_layout(root)
-        self.text = tk.Label(root, text= question)
+        Window.__init__(self)
+        self.text = tk.Label(root, text=question)
         self.text.place(relx=.5, rely=.3, anchor=CENTER)
         self.entry = tk.Entry(root, width=3)
         self.entry.place(relx=.5, rely=.5, anchor=CENTER)
@@ -28,8 +34,8 @@ class IntegerWindow(Window):
 # Type of window that shows the user different options to choose from (for example gender, yes/no or reading preferences)
 class CategoryWindow(Window):
     def __init__(self, question: str, categories: list) -> None:
-        root.geometry = self.basic_layout(root)
-        self.text = tk.Label(root, text= question)
+        Window.__init__(self)
+        self.text = tk.Label(root, text=question)
         self.text.place(relx=0.5, rely=.3, anchor=CENTER)
         self.categories = categories
         self.category_variables = [tk.IntVar]*len(categories)
@@ -39,9 +45,10 @@ class CategoryWindow(Window):
 
     def create_checkboxes(self):
         for category, category_variable in zip(self.categories, self.category_variables):
-            self.boxes.append(tk.Checkbutton(root, text=category, variable=category_variable))
+            self.boxes.append(tk.Checkbutton(
+                root, text=category, variable=category_variable))
 
-    # This is terrible, but havent found a better way for now    
+    # This is terrible, but havent found a better way for now
     def place_checkboxes(self):
         if len(self.boxes) == 2:
             self.boxes[0].place(relx=.40, rely=.5)
@@ -59,6 +66,7 @@ class CategoryWindow(Window):
             self.boxes[5].place(relx=.40, rely=.6)
             self.boxes[6].place(relx=.60, rely=.6)
             self.boxes[7].place(relx=.80, rely=.6)
+
 
 root = tk.Tk()
 root.title("Book Recommendation")
