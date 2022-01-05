331709845b2999cf2bcd0747be78c17a0ec5a749
mau-p <thisismyemail.me@pm.me>
2022-01-05 22:12:29 +0100

    Interface and Performance upgrades

--- GUI.py
+++ GUI.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import Text, font
-from tkinter.constants import CENTER, DISABLED, INSERT
+from tkinter.constants import CENTER, DISABLED, INSERT, TOP
 import questions
 
 
@@ -11,9 +10,8 @@ def incrementor():
         incrementor.num = 0         # initialize on first call
     return incrementor.num
 
+
 # Basic properties for all windows
-
-
 class Window:
     def __init__(self) -> None:
         self.background_color = "slate gray"
@@ -28,92 +26,117 @@ class Window:
         self.geometry = root.geometry(
             '%dx%d+%d+%d' % (self.window_width, self.window_height, self.x_pos, self.y_pos))
         self.previous = tk.Button(
-            root, text="Previous", bg="red3", activebackground="red", highlightbackground=self.highlight_background, fg=self.text_color,\
-                 width=10, command=lambda: self.previous_question())
+            root, text="Previous", bg="red3", activebackground="red", highlightbackground=self.highlight_background, fg=self.text_color,
+            width=10, command=lambda: self.go_back())
         self.previous.place(relx=.05, rely=.9)
-        self.next = tk.Button(root, text="Next", bg="forest green", activebackground="green yellow", fg=self.text_color, \
-            highlightbackground=self.highlight_background, width=10, command=lambda: self.next_question())
+        self.next = tk.Button(root, text="Next", bg="forest green", activebackground="green yellow", fg=self.text_color,
+                              highlightbackground=self.highlight_background, width=10, command=lambda: self.next_slide())
         if self.index == questions.get_question_list_length()-1:
             self.next.configure(state=DISABLED)
         self.next.place(relx=.85, rely=.9)
+        self.no_answer_label1 = tk.Label(
+            root, text="In order to find your ideal book, we need your answer to this question", fg='red', font=("Arial", 15))
+
+    def move_on(self, next_question, no_answer):
+        global app
+        if next_question.answer_type == "value":
+            app = IntegerWindow(next_question.question_text, no_answer)
+        if next_question.answer_type == "category":
+            app = CategoryWindow(next_question.question_text,
+                                 next_question.possible_answers, no_answer)
+
+    def go_back(self):
+        global app
+        self.destroy_window()
+        question = questions.get_previous_question()
+        if question.answer_type == "value":
+            app = IntegerWindow(question.question_text, False)
+        if question.answer_type == "category":
+            app = CategoryWindow(question.question_text,
+                                 question.possible_answers, False)
+        if question.answer_type == "welcome":
+            app = IntroWindow()
+
+    def check_answer(self, wrong_answer: bool, current_question, next_question):
+        if wrong_answer:
+            no_answer = True
+            next_question = current_question
+            questions.revert_question()
+        else:
+            no_answer = False
+        return next_question, no_answer
+
 
 # The window that is displayed when the user first launches the program
-
-
 class IntroWindow(Window):
     def __init__(self) -> None:
         Window.__init__(self)
         self.title = tk.Label(root, text="Welcome to BookRecommendation!", font=(
             'Arial', 30), bg=self.background_color, fg=self.text_color)
         self.presentation = tk.Label(root, text=("This knowledge system uses expert knowledge to give you a book recommendation. \n"
-                                                 "Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n"),\
-                                                      bg=self.background_color, fg=self.text_color, font=("Arial", 12))
+                                                 "Developed by Matthew Melcherts, Maurits Merks and Julius Wagenbach. \n"),
+                                     bg=self.background_color, fg=self.text_color, font=("Arial", 12))
         self.presentation.place(relx=.5, rely=.4, anchor=CENTER)
         self.title.place(relx=.5, rely=.3, anchor=CENTER)
         self.previous.configure(state=DISABLED)
 
     # Retrieves index of next question, and makes a window depending on type
-    def next_question(self):
+    def next_slide(self):
         global app
-        self.title.place_forget()
-        self.presentation.place_forget()
+        self.title.destroy()
+        self.presentation.destroy()
         _, question = questions.get_next_question()
         if question.answer_type == "value":
-            app = IntegerWindow(question.question_text)
+            app = IntegerWindow(question.question_text, False)
         if question.answer_type == "category":
             app = CategoryWindow(question.question_text,
-                                 question.possible_answers)
+                                 question.possible_answers, False)
 
 
 # Type of window that asks the user for value (for example their age or a book rating)
 class IntegerWindow(Window):
-    def __init__(self, question: str) -> None:
+    def __init__(self, question: str, no_answer: bool) -> None:
         Window.__init__(self)
-        self.text = tk.Label(root, text=question, bg=self.background_color, fg=self.text_color, font=("Arial", 14))
+        self.text = tk.Label(root, text=question, bg=self.background_color,
+                             fg=self.text_color, font=("Arial", 14))
         self.text.place(relx=.5, rely=.3, anchor=CENTER)
-        self.entry = tk.Entry(root, width=5, highlightbackground=self.highlight_background, justify=CENTER)
+        self.entry = tk.Entry(
+            root, width=5, highlightbackground=self.highlight_background, justify=CENTER)
         self.entry.insert(2, 20)
         self.entry.place(relx=.5, rely=.5, anchor=CENTER)
+        if no_answer:
+            self.no_answer_label1.pack(side=TOP)
+
+    def destroy_window(self):
+        global app
+        self.text.destroy()
+        self.entry.destroy()
+        self.no_answer_label1.destroy()
 
     # Retrieves index of next question, and makes a window depending on type
-    def next_question(self):
+    def next_slide(self):
         global app
-        self.text.place_forget()
-        self.entry.place_forget()
         current_question, next_question = questions.get_next_question()
-        if next_question.answer_type == "value":
-            app = IntegerWindow(next_question.question_text)
-        if next_question.answer_type == "category":
-            app = CategoryWindow(next_question.question_text,
-                                 next_question.possible_answers)
         current_question.answer = int(self.entry.get())
+        next_question, no_answer = self.check_answer(current_question.answer < 0 or type(
+            current_question.answer) is not int, current_question, next_question)
+        self.destroy_window()
+        self.move_on(next_question, no_answer)
 
-    # Retrieves index of previous question, and makes a window depending on type
-    def previous_question(self):
-        global app
-        self.text.place_forget()
-        self.entry.place_forget()
-        question = questions.get_previous_question()
-        if question.answer_type == "value":
-            app = IntegerWindow(question.question_text)
-        if question.answer_type == "category":
-            app = CategoryWindow(question.question_text,
-                                 question.possible_answers)
-        if question.answer_type == "welcome":
-            app = IntroWindow()
 
 # Type of window that shows the user different options to choose from (for example gender, yes/no or reading preferences)
-
-
 class CategoryWindow(Window):
-    def __init__(self, question: str, categories: list) -> None:
+    def __init__(self, question: str, categories: list, no_answer: bool) -> None:
         Window.__init__(self)
-        self.text = tk.Label(root, text=question, bg=self.background_color, fg=self.text_color, font=("Arial", 14))
+        self.text = tk.Label(root, text=question, bg=self.background_color,
+                             fg=self.text_color, font=("Arial", 14))
         self.text.place(relx=0.5, rely=.3, anchor=CENTER)
         self.categories = categories
         self.boxes = []
         self.checkbox = tk.IntVar(value=0)
         self.init_checkboxes()
+        if no_answer:
+            self.no_answer_label1.pack(side=TOP)
 
     def init_checkboxes(self):
         for i in range(len(self.categories)):
@@ -128,49 +151,28 @@ class CategoryWindow(Window):
             self.boxes[1].place(relx=.46, rely=.5)
             self.boxes[2].place(relx=.67, rely=.5)
 
+    def destroy_window(self):
+        self.text.destroy()
+        self.no_answer_label1.pack_forget()
+        for box in self.boxes:
+            box.destroy()
+
     # Retrieves index of next question, and makes a window depending on type
-    def next_question(self):
+    def next_slide(self):
         global app
-        self.text.place_forget()
-        for box in self.boxes:
-            box.place_forget()
+        self.destroy_window()
         current_question, next_question = questions.get_next_question()
-        if self.checkbox.get() == 0:  # Checks if the questions has been answered
-            next_question = current_question
-            questions.revert_question()
-            answer_warning_label = tk.Label(root, text="In order to find your ideal book, we need your answer to this question. \n \
-                If you don't know the answer, you can answer neutral in most cases", fg='red')
-            answer_warning_label.place(relx=0.5, rely=0.1, anchor=CENTER)
-        current_question.answer = self.categories[self.checkbox.get()-1]
-        if next_question.answer_type == "value":
-            app = IntegerWindow(next_question.question_text)
-        if next_question.answer_type == "category":
-            app = CategoryWindow(next_question.question_text,
-                                 next_question.possible_answers)
-
-    # Retrieves index of previous question, and makes a window depending on type
-
-    def previous_question(self):
-        global app
-        self.text.place_forget()
-        for box in self.boxes:
-            box.place_forget()
-        question = questions.get_previous_question()
-        if question.answer_type == "value":
-            app = IntegerWindow(question.question_text)
-        if question.answer_type == "category":
-            app = CategoryWindow(question.question_text,
-                                 question.possible_answers)
-        if question.answer_type == "welcome":
-            app = IntroWindow()
+        next_question, no_answer = self.check_answer(self.checkbox.get() == 0, current_question, next_question)
+        self.move_on(next_question, no_answer)
 
 
 class SummaryWindow(Window):
     def __init__(self, title: str, author: str, summary: str) -> None:
         Window.__init__(self)
         self.question = tk.Label(
-            root, text="What do you think about this book?", bg=self.background_color, fg=self.text_color, font =("Arial", 14))
-        self.title = tk.Label(root, text=title, font=("Arial", 25), bg=self.background_color, fg=self.text_color)
+            root, text="What do you think about this book?", bg=self.background_color, fg=self.text_color, font=("Arial", 14))
+        self.title = tk.Label(root, text=title, font=(
+            "Arial", 25), bg=self.background_color, fg=self.text_color)
         self.author = tk.Label(root, text=f'by {author}', bg="green2")
         self.textbox = tk.Text(root, height=13, width=110, wrap=tk.WORD)
         self.textbox.insert(INSERT, summary)
@@ -180,16 +182,34 @@ class SummaryWindow(Window):
         self.title.place(relx=.5, rely=.25, anchor=CENTER)
         self.author.place(relx=.5, rely=.30, anchor=CENTER)
         self.checkbox = tk.IntVar()
-        self.would = tk.Checkbutton(root, text="I would read this", onvalue=1, variable=self.checkbox, width=20, height=2, \
-             highlightbackground=self.highlight_background)
-        self.would_not = tk.Checkbutton(root, text="I would not read this", onvalue=1, variable=self.checkbox, width=20, height=2, \
-             highlightbackground=self.highlight_background)
+        self.would = tk.Checkbutton(root, text="I would read this", onvalue=1, variable=self.checkbox, width=20, height=2,
+                                    highlightbackground=self.highlight_background)
+        self.would_not = tk.Checkbutton(root, text="I would not read this", onvalue=1, variable=self.checkbox, width=20, height=2,
+                                        highlightbackground=self.highlight_background)
         self.would_not.place(relx=.25, rely=.75)
         self.would.place(relx=.55, rely=.75)
-        
 
-root = tk.Tk()
-root.title("Book Recommendation")
-app = IntroWindow()
-# app = SummaryWindow("This is a title", "Author", "Summary")
-root.mainloop()
+    def destroy_window(self):
+        self.question.destroy()
+        self.title.destroy()
+        self.textbox.destroy()
+        self.would.place()
+        self.would_not.destroy()
+        self.author.destroy()
+
+    def next_slide(self):
+        global app
+        self.destroy_window()
+        current_question, next_question = questions.get_next_question()
+        next_question, no_answer = self.check_answer(
+            self.checkbox == 0, current_question, next_question)
+        self.move_on(next_question, no_answer)
+
+def launch_GUI():
+    global root
+    global app
+    root = tk.Tk()
+    root.title("Book Recommendation")
+    app = IntroWindow()
+    root.mainloop()
+    
--- questions.py
+++ questions.py
@@ -7,78 +7,52 @@ class Question:
         self.possible_answers = possible_answers
         self.answer = None  # The answer to the question
 
-
-# Not an ideal implementation, but does for now
-welcome = Question(None, "welcome", None)
-age_question = Question("How old are you?", "value", None)
-gender_question = Question("I am a", "category", ["Man", "Neutral", "Woman"])
-like_complex = Question("I enjoy complex language", "category", [
-                        "Disagree", "Neutral", "Agree"])
-like_simple_narratives = Question("Complex narratives with many subplots are often too complicate for me to enjoy", "category", [
-                                  "Disagree", "Neutral", "Agree"])
-reading_task = Question("I dont like it if reading feels like a task", "category", [
-                        "Disagree", "Neutral", "Agree"])
-regular_reader = Question("I consider myself a regular reader", "category", [
-                          "Disagree", "Neutral", "Agree"])
-read_more = Question("I would like to read more books then I currently do", "category", [
-    "Disagree", "Neutral", "Agree"])
-humor = Question("I dont mind if a book does not have humoristic elements", "category", [
-    "Disagree", "Neutral", "Agree"])
-comedy_v_drama = Question("I generally like comedy over drama", "category", [
-                          "Disagree", "Neutral", "Agree"])
-happy_ending = Question("I generally prefer it if stories have a happy ending", "category", [
-    "Disagree", "Neutral", "Agree"])
-relax = Question("A main reason I like to read is because it helps me to relax and unwind", "category", [
-    "Disagree", "Neutral", "Agree"])
-challenge = Question("I like it if books feel like a challenge", "category", [
-    "Disagree", "Neutral", "Agree"])
-social_issues = Question("I often think about social issues", "category", [
-    "Disagree", "Neutral", "Agree"])
-history = Question("I generally enjoy history", "category", [
-    "Disagree", "Neutral", "Agree"])
-intense = Question("I like intense stories", "category", [
-    "Disagree", "Neutral", "Agree"])
-own_life = Question("I prefer books with stories that are similar to my own life", "category", [
-    "Disagree", "Neutral", "Agree"])
-leisure_pace = Question("I prefer reading at a leisurely pace", "category", [
-    "Disagree", "Neutral", "Agree"])
-real_world = Question("I like books that make me reflect upon the real world", "category", [
-    "Disagree", "Neutral", "Agree"])
-mysteries = Question("I enjoy it when there are mysteries in the plot", "category", [
-    "Disagree", "Neutral", "Agree"])
-romantic = Question("I would consider myself a romantic person", "category", [
-    "Disagree", "Neutral", "Agree"])
-finishing = Question("I don't like not finishing books", "category", [
-    "Disagree", "Neutral", "Agree"])
-moody = Question("I dont like it if characters are overly moody and sentimental", "category", [
-    "Disagree", "Neutral", "Agree"])
-
-
-# TODO: Find a better implementation of whatever this is.
-question_list = []
-question_list.append(welcome)
-question_list.append(age_question)
-question_list.append(gender_question)
-question_list.append(like_complex)
-question_list.append(like_simple_narratives)
-question_list.append(reading_task)
-question_list.append(regular_reader)
-question_list.append(read_more)
-question_list.append(humor)
-question_list.append(comedy_v_drama)
-question_list.append(happy_ending)
-question_list.append(relax)
-question_list.append(challenge)
-question_list.append(social_issues)
-question_list.append(history)
-question_list.append(intense)
-question_list.append(own_life)
-question_list.append(leisure_pace)
-question_list.append(real_world)
-question_list.append(mysteries)
-question_list.append(romantic)
-question_list.append(finishing)
-question_list.append(moody)
+def initialize_questions():
+    global question_list
+    question_list=[]
+    question_list.append(Question(None, "welcome", None))
+    question_list.append(Question("How old are you?", "value", None))
+    question_list.append(Question("I am a", "category", ["Man", "Neutral", "Woman"]))
+    question_list.append(Question("I enjoy complex language", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("Complex narratives with many subplots are often too complicate for me to enjoy", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I dont like it if reading feels like a task", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I consider myself a regular reader", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I would like to read more books then I currently do", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I dont mind if a book does not have humoristic elements", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I generally like comedy over drama", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I generally prefer it if stories have a happy ending", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("A main reason I like to read is because it helps me to relax and unwind", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I like it if books feel like a challenge", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I often think about social issues", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I generally enjoy history", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I like intense stories", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I prefer books with stories that are similar to my own life", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I prefer reading at a leisurely pace", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I like books that make me reflect upon the real world", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I enjoy it when there are mysteries in the plot", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I would consider myself a romantic person", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append( Question("I don't like not finishing books", "category", [
+                        "Disagree", "Neutral", "Agree"]))
+    question_list.append(Question("I dont like it if characters are overly moody and sentimental", "category", [
+                        "Disagree", "Neutral", "Agree"]))
 
 # Functions for next and previous buttons
 question_ref = 1
