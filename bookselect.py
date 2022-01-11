217b8ebcc86b5a4c233bdcaf4da7450fb09db932
jwagenbach <ju.wa@web.de>
2022-01-11 15:46:01 +0100

    improved the scoring

--- bookselect.py
+++ bookselect.py
@@ -14,18 +14,24 @@ def get_recommendations(preferences):
         properties = bookmanagement.get_properties(cursor, ID)
         #gender 
         if question_list[2].answer == properties[0] or question_list[2].answer == "other" or properties[0] == "A":
-            score+=1 
+            score+=1
+        else: 
+            score-=1
         #age 
         if question_list[1].answer >= properties[1]:
             score+=1
+        else:
+            score-=1
         #reading level 
         if properties[2]-2 <= preferences[0]: 
             score+=1
-    
+        else:
+            score-=1
         for index in range(2, len(properties)):
-            if properties[index] == preferences[index-2]:
+            if properties[index] == 1 and preferences[index-2] == 1:
                 score += 1
-
+            elif properties[index] == 0 and preferences[index-2] == -1:
+                score -= 1
         scores.append((score, ID))
 
     scores.sort(key=lambda x: x[0], reverse=True)
